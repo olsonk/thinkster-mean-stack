50e24ae729b6426cd0e16c08cd44716ade0eea6a
Kevin Olson <olsonk@gmail.com>
2015-07-09 16:36:38 -0700

    Completed up to 'Creating New Comments'

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
 'use strict';
 
-var app = angular.module('FlapperNews', []);
+var app = angular.module('FlapperNews', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+    })
+    .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+    });
+    
+    $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', function() {
+    var o = {
+       posts: []
+    };
+    return o;
+});
+
+app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
     $scope.test = "Hello World!";
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 0},
-    ];
+    $scope.posts = posts.posts;
     $scope.addPost = function () {
         if (!$scope.title || $scope.title === "") {
             return;
         }
-        $scope.posts.push({title: $scope.title, upvotes: 0});
+        $scope.posts.push({
+            title: $scope.title, 
+            link: $scope.link, 
+            upvotes: 0,
+            comments: [
+                {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                {author: "Bob", body: "Great idea but everything is wrong!", upvotes: 0}
+            ]
+        });
         $scope.title = "";
+        $scope.link = "";
     };
     $scope.incrementUpvotes = function (post) {
         post.upvotes += 1;
+    };
+}]);
+
+app.controller('PostsCtrl', 
+               ['$scope', 'posts', '$stateParams', function ($scope, posts, $stateParams) {
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.incrementUpvotes = function (commentId) {
+        $scope.post.comments[commentId].upvotes += 1;
     };
 }]);
